4708b8c927bf0a2455359ddb7cbf32e6bef14b4b
Felix <floresfelixleo.fleurs96@gmail.com>
2026-06-11 00:36:30 +0800

    chore: bumped version, added doc, added classes from fields.py

--- src/django_dataclassconf/__init__.py
+++ src/django_dataclassconf/__init__.py
@@ -1,9 +1,16 @@
-__version__ = '0.1.0'
+"""
+Typed, dataclass-based configuration management for Django.
+"""
+
+__version__ = '0.2.0'
 
 from .conf import BaseConfig, config_loader
+from .fields import Importable, ImportableValue
 
 __all__ = [
     "BaseConfig", 
     "config_loader", 
+    "Importable", 
+    "ImportableValue", 
     "__version__",
 ]
