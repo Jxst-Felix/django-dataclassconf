efe957e67028da34b0a88517a43cfb517a0fda70
Felix <floresfelixleo.fleurs96@gmail.com>
2026-09-07 04:39:42 +0800

    test: added new test cases

--- test/appconf_test/cust_fields.py
+++ test/appconf_test/cust_fields.py
@@ -1,50 +1,40 @@
-from django_dataclassconf.fields import Field, FieldGeneric, FieldValue
+from django_dataclassconf.fields import Field, FieldGeneric, FieldValue, field_type
 import warnings
 import typing
+import re
 
 
 class DeprecatedValue(FieldValue):
-    def __init__(self, value: typing.Any, inner_type: typing.Type[typing.Any]):
-        self.value = value
-        self.inner_type: typing.Type[typing.Any] = inner_type
-
     def validate(self):
         if not isinstance(self.value, self.inner_type):
             raise TypeError(f'Instance {self.value} is not of type {self.inner_type.__name__}')
         warnings.warn(f'This setting is depricated', DeprecationWarning, stacklevel = 2)
-    
-    def resolve(self):
-        return self.value
-    
-    def __repr__(self):
-        return repr(self.value)
-    
-    def __eq__(self, value):
-        if isinstance(value, DeprecatedValue):
-            return (
-                self.value == value.value 
-                and self.inner_type == value.inner_type
-            )
-        
-        if isinstance(value, self.inner_type):
-            return self.value == value
-        return NotImplemented
-    
-    def __hash__(self):
-        return hash((repr(self.value), repr(self.inner_type)))
 
+class _DeprecatedGeneric(FieldGeneric):
+    _value_class = DeprecatedValue
 
-class DeprecatedGeneric(FieldGeneric):
-    def __repr__(self):
-        return f'Deprecated[{self.__inner_type__}]'
-
-    def instanciate(self, value):
-        return DeprecatedValue(value, self.__inner_type__)
-
+class _DeprecatedField(Field):
+    _generic_class = _DeprecatedGeneric
 
 if typing.TYPE_CHECKING:
     Deprecated = DeprecatedValue
 
 else:
-    class Deprecated(Field):
-        _generic_class = DeprecatedGeneric
+    Deprecated = _DeprecatedField
+
+
+@field_type()
+class Email(FieldValue):
+    def validate(self):
+        email_pattern = r'^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'
+        if not re.match(email_pattern, self.value):
+            raise ValueError(f'Email value {self.value} is not a valid email address')
+
+    def __str__(self):
+        return self.value
+
+    def __eq__(self, value):
+        if isinstance(value, str):
+            return self.value == value
+        return super().__eq__(value)
+    
--- test/appconf_test/tests.py
+++ test/appconf_test/tests.py
@@ -1,15 +1,172 @@
 from django.test import SimpleTestCase, override_settings
 from django.contrib.auth.models import User
 
-from django_dataclassconf.conf import BaseConfig, config_loader
-from django_dataclassconf.fields import ImportableValue, PathValue
+from django_dataclassconf.core import BaseConfig, config_loader
+from django_dataclassconf.fields import (
+    FieldGeneric, 
+    FieldValue, 
+    Importable, 
+    Field, 
+    Path,
+)
 
 from dacite.exceptions import WrongTypeError
 
 import pathlib
+import typing
 
-from appconf_test.config import TestConfig, Test2Config, Renderer, new_as_dict
 from appconf_test.models import UserSubclass
+from appconf_test.config import (
+    Test2Config,
+    new_as_dict,
+    TestConfig,
+    Renderer,
+)
+
+from appconf_test.cust_fields import Email, DeprecatedValue
+
+
+class NativeFieldTests(SimpleTestCase):
+    # Importable
+    def test_importable_field_is_generated(self):
+        self.assertTrue(issubclass(Importable, Field))
+
+    def test_importable_is_parameterized(self):
+        possible_inner_types = [
+            int,
+            typing.Callable, 
+            typing.Type,
+            typing.Callable[[str], bool],
+        ]
+
+        for possible_field_type in possible_inner_types:
+            field_type = Importable[possible_field_type]
+            self.assertIsInstance(field_type, FieldGeneric)
+            self.assertIs(field_type.__origin__, Importable)
+            self.assertIs(field_type.__inner_type__, possible_field_type)
+
+    def test_valid_import(self):
+        importables = [
+            (TestConfig, 'appconf_test.config.configuration'), # import var
+            (typing.Type[BaseConfig], 'appconf_test.config.TestConfig'), # import class
+            (typing.Type[User], 'appconf_test.UserSubclass'), # import Model
+            (typing.Callable[[str], bool], 'appconf_test.config.foo_bar'), # import function
+        ]
+
+        for (inner_type, value) in importables:
+            importable: Importable = Importable[inner_type].instantiate(value)
+            self.assertTrue(importable.is_valid)
+
+    def test_invalid_import(self):
+        importables = [
+            (TestConfig, 'appconf_test.config.non_existent'), # import var
+            (typing.Type[BaseConfig], 'appconf_test.config.NonExistentConfig'), # import class
+            (typing.Type[User], 'appconf_test.NotUserSubclass'), # import Model
+            (typing.Callable[[str], bool], 'appconf_test.config.not_foo_bar'), # import function
+        ]
+        
+        for (inner_type, value) in importables:
+            importable: Importable = Importable[inner_type].instantiate(value)
+            with self.assertRaises((ValueError, AttributeError, ImportError, TypeError)):
+                importable.validate()
+
+    def test_importable_equality(self):
+        importables = [
+            (TestConfig, 'appconf_test.config.configuration'), # import var
+            (typing.Type[BaseConfig], 'appconf_test.config.TestConfig'), # import class
+            (typing.Type[User], 'appconf_test.UserSubclass'), # import Model
+            (typing.Callable[[str], bool], 'appconf_test.config.foo_bar'), # import function
+        ]
+
+        for (inner_type, value) in importables:
+            importable: Importable = Importable[inner_type].instantiate(value)
+            self.assertEqual(importable, Importable[inner_type].instantiate(value))
+            self.assertEqual(importable, value)
+            self.assertNotEqual(importable, 'this_is.not.Importable')
+
+    # Path
+    def test_path_field_is_generated(self):
+        self.assertTrue(issubclass(Path, Field))
+
+    @property
+    def _path_generic(self):
+        return Path[str]
+
+    def _create_path_value(self, path_str: str) -> Path:
+        return self._path_generic.instantiate(path_str)
+
+    def test_path_is_parameterized(self):
+        field_type = self._path_generic
+        self.assertIsInstance(field_type, FieldGeneric)
+        self.assertIs(field_type.__origin__, Path)
+        self.assertIs(field_type.__inner_type__, str)
+
+    def test_valid_path(self):
+        path_str = '/absolute/path/'
+        path_value = self._create_path_value(path_str)
+        self.assertTrue(path_value.is_valid)
+        self.assertEqual(path_value.resolve(), pathlib.Path(path_str))
+
+    def test_invalid_path(self):
+        path_str = 123
+        path_value = self._create_path_value(path_str)
+        self.assertFalse(path_value.is_valid)
+        with self.assertRaises(TypeError):
+            path_value.resolve()
+
+    def test_path_equality(self):
+        path_value = self._create_path_value('/a/b')
+        self.assertEqual(path_value, '/a/b')
+        self.assertEqual(path_value, self._create_path_value('/a/b'))
+        self.assertNotEqual(path_value, '/a/c')
+        self.assertNotEqual(path_value, self._create_path_value('/a/c'))
+
+
+class CustomFieldTests(SimpleTestCase):
+    # Deprecation
+    def test_deprecated_field_raise_warning(self):
+        deprecated = DeprecatedValue(67, int)
+        with self.assertWarns(DeprecationWarning):
+            deprecated.validate()
+
+    def test_deprecated_field_type_mismatch(self):
+        deprecated = DeprecatedValue('six-seven', int)
+        with self.assertRaises(TypeError):
+            deprecated.validate()
+
+    # Email
+    @property
+    def _email_generic(self):
+        return Email[str]
+
+    def _create_email_value(self, email_str: str) -> Email:
+        return self._email_generic.instantiate(email_str)
+
+    def test_email_field_is_generated(self):
+        self.assertTrue(issubclass(Email, Field))
+    
+    def test_email_field_is_parameterized(self):
+        field_type = self._email_generic
+        self.assertIsInstance(field_type, FieldGeneric)
+        self.assertIs(field_type.__origin__, Email)
+        self.assertIs(field_type.__inner_type__, str)
+
+    def test_valid_email(self):
+        email = self._create_email_value("admin_project@domain.com")
+        self.assertTrue(email.is_valid)
+        self.assertEqual(email.resolve(), "admin_project@domain.com")
+
+    def test_invalid_email(self):
+        email = self._create_email_value("not-an-email")
+        with self.assertRaises(ValueError) as context:
+            email.validate()
+        self.assertEqual(str(context.exception), "Email value not-an-email is not a valid email address")
+
+    def test_email_equality(self):
+        email = self._create_email_value("admin_project@domain.com")
+        self.assertEqual(email, self._create_email_value("admin_project@domain.com"))
+        self.assertEqual(email, "admin_project@domain.com")
+        self.assertNotEqual(email, "other@domain.com")
 
 
 class DataclassConfSignalTests(SimpleTestCase):
@@ -18,13 +175,17 @@ class DataclassConfSignalTests(SimpleTestCase):
         self.config = TestConfig()
         config_loader.subscribe(self.config)
 
+    @override_settings(TEST = {'PROJECT_EMAIL': 'another_email@next.ph'})
+    def test_email_config_resolve(self):
+        self.assertEqual(self.config.PROJECT_EMAIL.resolve(), 'another_email@next.ph')
+
     @override_settings(TEST_MAX_RETRIES = 24)
     def test_flat_config(self):
         self.assertEqual(self.config.MAX_RETRIES, 24)
 
     @override_settings(TEST = {'DEFAULT_PATH': '/a/path/to/somewhere'})
     def test_block_config(self):
-        self.assertEqual(self.config.DEFAULT_PATH, '/a/path/to/somewhere')
+        self.assertEqual(self.config.DEFAULT_PATH.resolve(), '/a/path/to/somewhere')
 
     @override_settings(TEST = {'RENDERER': {'refresh_rate': 24}})
     def test_nested_config(self):
@@ -37,17 +198,27 @@ class DataclassConfSignalTests(SimpleTestCase):
                 pass
         self.assertIn('MAX_RETRIES', str(error.exception))
 
-    def test_importable_fields_are_importable_value_after_subscribe(self):
-        self.assertIsInstance(self.config.CONFIG_SUBCLASS, ImportableValue)
-        self.assertIsInstance(self.config.CONFIG_INSTANCE, ImportableValue)
-        self.assertIsInstance(self.config.USER_MODEL, ImportableValue)
-        self.assertIsInstance(self.config.DATACLASS_AS_DICT, ImportableValue)
+    def test_importable_fields_are_field_value_subclass_after_subscribe(self):
+        fvs = [
+            self.config.CONFIG_SUBCLASS,
+            self.config.CONFIG_INSTANCE, 
+            self.config.USER_MODEL, 
+            self.config.DATACLASS_AS_DICT,
+        ]
+
+        for fv in fvs:
+            self.assertIsInstance(fv, FieldValue)
 
     def test_importable_value_stores_originalimport_string(self):
-        self.assertEqual(self.config.CONFIG_SUBCLASS.import_str, 'appconf_test.config.TestConfig')
-        self.assertEqual(self.config.CONFIG_INSTANCE.import_str, 'appconf_test.config.configuration')
-        self.assertEqual(self.config.USER_MODEL.import_str, 'auth.User')
-        self.assertEqual(self.config.DATACLASS_AS_DICT.import_str, 'dataclasses.dataclass.asdict')
+        importables = [
+            (self.config.CONFIG_SUBCLASS, 'appconf_test.config.TestConfig'),
+            (self.config.CONFIG_INSTANCE, 'appconf_test.config.configuration'), 
+            (self.config.USER_MODEL, 'auth.User'), 
+            (self.config.DATACLASS_AS_DICT, 'dataclasses.dataclass.asdict'),
+        ]
+
+        for (fv, value) in importables:
+            self.assertEqual(fv.value, value)
 
     def test_resolve_config_subclass(self):
         resolved = self.config.CONFIG_SUBCLASS.resolve()
@@ -81,39 +252,15 @@ class DataclassConfSignalTests(SimpleTestCase):
         with self.assertRaises(ImportError):
             self.config.DATACLASS_AS_DICT.resolve()
 
-    def test_resolve_type_mismatch_subclass_raises_type_error(self):
-        iv = ImportableValue('builtins.str', type[BaseConfig])
-        with self.assertRaises(TypeError) as ctx:
-            iv.resolve()
-        self.assertIn('subclass', str(ctx.exception))
-
-    def test_resolve_instance_mismatch_raises_type_error(self):
-        iv = ImportableValue('builtins.int', BaseConfig)
-        with self.assertRaisesRegex(
-            TypeError, r'Resolving builtins.int: expected a subclass of'
-        ):
-            iv.resolve()
-
-    def test_resolve_bad_attr_raises_import_error(self):
-        iv = ImportableValue('builtins.NonExistent', object)
-        with self.assertRaises(ImportError) as ctx:
-            iv.resolve()
-        self.assertIn('NonExistent', str(ctx.exception))
-
-    def test_resolve_bare_name_raises_value_error(self):
-        iv = ImportableValue('TestConfig', type[BaseConfig])
-        with self.assertRaises((ValueError, ImportError)):
-            iv.resolve()
-
     def test_override_replaces_importable_value(self):
         original_iv = self.config.CONFIG_SUBCLASS
         with override_settings(TEST_CONFIG_SUBCLASS = 'appconf_test.config.Test2Config'):
             updated_iv = self.config.CONFIG_SUBCLASS
-            self.assertIsInstance(updated_iv, ImportableValue)
-            self.assertEqual(updated_iv.import_str, 'appconf_test.config.Test2Config')
+            self.assertIsInstance(updated_iv, FieldValue)
+            self.assertEqual(updated_iv.value, 'appconf_test.config.Test2Config')
             self.assertIsNot(updated_iv, original_iv)
 
-        self.assertEqual(self.config.CONFIG_SUBCLASS.import_str, 'appconf_test.config.TestConfig')
+        self.assertEqual(self.config.CONFIG_SUBCLASS.value, 'appconf_test.config.TestConfig')
 
     def test_override_new_importable_value_resolves_correctly(self):
         with override_settings(TEST_CONFIG_SUBCLASS = 'appconf_test.config.Test2Config'):
@@ -123,7 +270,7 @@ class DataclassConfSignalTests(SimpleTestCase):
     def test_non_importable_field_unaffected_by_importable_override(self):
         with override_settings(TEST_CONFIG_SUBCLASS = 'appconf_test.config.Test2Config'):
             self.assertEqual(self.config.MAX_RETRIES, 5)
-            self.assertEqual(self.config.DEFAULT_PATH, '/root/default/path/')
+            self.assertEqual(self.config.DEFAULT_PATH.resolve(), '/root/default/path/')
 
     def test_importable_and_plain_field_override_together(self):
         with override_settings(
@@ -132,34 +279,16 @@ class DataclassConfSignalTests(SimpleTestCase):
         ):
             self.assertEqual(self.config.MAX_RETRIES, 99)
             self.assertEqual(
-                self.config.CONFIG_SUBCLASS.import_str,
+                self.config.CONFIG_SUBCLASS.value,
                 'appconf_test.config.Test2Config',
             )
-
-    def test_importable_value_equality(self):
-        a = ImportableValue('appconf_test.config.TestConfig', type[BaseConfig])
-        b = ImportableValue('appconf_test.config.TestConfig', type[BaseConfig])
-        self.assertEqual(a, b)
-
-    def test_importable_value_inequality_different_path(self):
-        a = ImportableValue('appconf_test.config.TestConfig', type[BaseConfig])
-        b = ImportableValue('appconf_test.config.Test2Config', type[BaseConfig])
-        self.assertNotEqual(a, b)
-
-    def test_importable_value_is_hashable(self):
-        iv = ImportableValue('appconf_test.config.TestConfig', type[BaseConfig])
-        self.assertIsInstance(hash(iv), int)
-
-    def test_importable_value_repr_contains_path(self):
-        iv = ImportableValue('appconf_test.config.TestConfig', type[BaseConfig])
-        self.assertIn('appconf_test.config.TestConfig', repr(iv))
 
     def test_field_deprecation_subclass(self):
         with self.assertWarns(DeprecationWarning):
             self.config.DEFAULT_PATH.validate()
 
     def test_path_field_is_path_value_after_subscribe(self):
-        self.assertIsInstance(self.config.MEDIA_ROOT, PathValue)
+        self.assertIsInstance(self.config.MEDIA_ROOT, FieldValue)
 
     def test_path_resolve_returns_pathlib_path(self):
         resolved = self.config.MEDIA_ROOT.resolve()
@@ -168,7 +297,7 @@ class DataclassConfSignalTests(SimpleTestCase):
 
     def test_path_override_via_flat_setting(self):
         with override_settings(TEST_MEDIA_ROOT='/custom/media/root'):
-            self.assertIsInstance(self.config.MEDIA_ROOT, PathValue)
+            self.assertIsInstance(self.config.MEDIA_ROOT, FieldValue)
             self.assertEqual(
                 self.config.MEDIA_ROOT.resolve(),
                 pathlib.Path('/custom/media/root'),
@@ -177,26 +306,3 @@ class DataclassConfSignalTests(SimpleTestCase):
             self.config.MEDIA_ROOT.resolve(),
             pathlib.Path('/root/default/path/'),
         )
-
-    def test_path_value_accepts_os_pathlike(self):
-        pv = PathValue(pathlib.Path('/already/a/path'), str)
-        self.assertTrue(pv.is_valid)
-        self.assertEqual(pv.resolve(), pathlib.Path('/already/a/path'))
-
-    def test_path_value_rejects_non_path_type(self):
-        pv = PathValue(123, str)
-        self.assertFalse(pv.is_valid)
-        with self.assertRaises(TypeError):
-            pv.resolve()
-
-    def test_path_value_equality_and_hash(self):
-        a = PathValue('/a/b', str)
-        b = PathValue('/a/b', str)
-        c = PathValue('/a/c', str)
-        self.assertEqual(a, b)
-        self.assertNotEqual(a, c)
-        self.assertEqual(hash(a), hash(b))
-
-    def test_path_value_repr_contains_value(self):
-        pv = PathValue('/a/b', str)
-        self.assertIn('/a/b', repr(pv))
